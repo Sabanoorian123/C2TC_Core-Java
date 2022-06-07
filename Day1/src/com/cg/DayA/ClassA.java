33deab8617aa861f8391c27969c2f5b96d2f246f
saba noorian <saba noorian@LAPTOP-LU092DG9>
2022-06-07 15:18:11 +0530

    second commit

---
+++ Day1/src/com/cg/DayA/ClassA.java
@@ -0,0 +1,15 @@
+package com.cg.DayA;
+
+public class ClassA {
+
+	    public static void main(String args[]) {
+	        int var1 = 5;
+	        int var2 = 6;
+	        if ((var2 = 1) == var1)
+	            System.out.print(var2);
+	        else
+	            System.out.print(++var2);
+	    }
+	}
+
+
