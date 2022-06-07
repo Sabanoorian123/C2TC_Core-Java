9e05d2c7f2eefc5c077c1788b377e11485985912
saba noorian <saba noorian@LAPTOP-LU092DG9>
2022-06-07 14:48:03 +0530

    Second commit

---
+++ Day1/src/com/cg/Day1/PreAndPostIncrement.java
@@ -0,0 +1,25 @@
+package com.cg.Day1;
+
+public class PreAndPostIncrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 5;
+
+		int b = 2;
+
+		int c;
+
+		int d;
+
+		c = ++b; // LINE A
+
+		d = a++; // LINE B
+
+		c++; // LINE C
+
+		System.out.println("a = " + a + " b = " + b + " c = " + c + " d = " + d);
+	}
+
+}
