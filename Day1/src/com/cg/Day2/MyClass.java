864bf83b24d574c7dcce660b0832e77c6501b175
saba noorian <saba noorian@LAPTOP-LU092DG9>
2022-06-07 15:08:24 +0530

    thrird commit

---
+++ Day1/src/com/cg/Day2/MyClass.java
@@ -0,0 +1,15 @@
+package com.cg.Day2;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=18;
+		if((a=3)==b)
+		{ System.out.println(++a);}
+		else
+		{ System.out.println(++b); }
+
+	}
+
+}
